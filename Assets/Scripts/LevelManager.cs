3b5ec9035a07c3fbcb9b97313c85b4f513a10b47
michael sjögren <mikel-carl@hotmail.com>
2017-01-30 09:55:19 +0100

    First commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    /** Load the level**/
+    public void LoadLevel(string name)
+    {
+        Debug.Log("scene: " + name + " loaded");
+            SceneManager.LoadScene(name);  
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("quit!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+    private int max;
+    private int min;
+    int guess;
+    public Text numberOfGuessesText;
+    public Text guessText;
+
+    int maxGuessesAllowed = 10;
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
+
+    public void StartGame()
+    {
+
+        max = 1000;
+        min = 1;
+        guess = Random.Range(min, max);
+        max += 1;
+        guessText.text = guess.ToString();
+
+    }
+    /** prints guess and instructions**/
+    void NextGuess()
+    {
+        guess = Random.Range(min - 1, max +1);
+        maxGuessesAllowed--;
+        guessText.text = guess.ToString();
+        updateMaxGuessText();
+
+        if (maxGuessesAllowed == 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+
+    public void GuessHighher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void updateMaxGuessText()
+    {
+        numberOfGuessesText.text = maxGuessesAllowed.ToString();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+ 
+}
